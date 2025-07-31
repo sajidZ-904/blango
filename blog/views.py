df62606ae94f2e4e6e77e51317d48f011b35343d
Sajid Zaman <zamansajid813@gmail.com>
2025-07-31 09:27:05 +0000

    Finish logging

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import logging
 
 # Create your views here.
+logger = logging.getLogger(__name__)
+
+logger.debug("Got %d posts", len(posts))
+
+logger.info(
+    "Created comment on Post %d for user %s", post.pk, request.user
+)
