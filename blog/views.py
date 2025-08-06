be3eb67b83eea1a553ab8355ebc6a5e45186f2e9
Sajid Zaman <zamansajid813@gmail.com>
2025-08-06 11:00:15 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers
+from django.http import HttpResponse
 import logging
 
 # Create your views here.
@@ -9,3 +12,11 @@ logger.debug("Got %d posts", len(posts))
 logger.info(
     "Created comment on Post %d for user %s", post.pk, request.user
 )
+
+@cache_page(300)
+@vary_on_headers("Cookie")
+def index(request):
+    return HttpResponse(str(request.user).encode("ascii"))
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
+    return render(request, "blog/index.html", {"posts": posts})
